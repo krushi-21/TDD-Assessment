2c5f77b1c7e28a39064bfb21056939f696a7c3a7
krushi-21 <krushi.thakkar111@gmail.com>
2024-10-02 20:36:09 +0530

    step-3 addded with test cases

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,6 +2,7 @@ import { StringCalculator } from "./index";
 
 const calculator = new StringCalculator();
 describe("String Calculator", () => {
+  //step 1-2 test cases
   it("should return 0 for an empty string", () => {
     expect(calculator.add("")).toBe(0);
   });
@@ -16,5 +17,22 @@ describe("String Calculator", () => {
 
   it("should return the sum of multiple numbers", () => {
     expect(calculator.add("1,2,3,4,5")).toBe(15);
+  });
+
+  //step-3 test cases
+  it("should handle new lines between numbers", () => {
+    expect(calculator.add("1\n2,3")).toBe(6);
+  });
+
+  it("should handle multiple new lines and commas between numbers", () => {
+    expect(calculator.add("1\n2\n3,4")).toBe(10);
+  });
+
+  it("should handle only new lines between numbers", () => {
+    expect(calculator.add("1\n2\n3")).toBe(6);
+  });
+
+  it("should return 0 for a string with only new lines and no numbers", () => {
+    expect(calculator.add("\n\n")).toBe(0);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export class StringCalculator {
   add(numbers: string): number {
     if (!numbers.trim()) return 0;
 
-    const numberArray = numbers.split(",").map((num) => {
+    const numberArray = numbers.split(/[\n,]/).map((num) => {
       const parsed = parseInt(num, 10);
       return isNaN(parsed) ? 0 : parsed;
     });
@@ -10,7 +10,3 @@ export class StringCalculator {
     return numberArray.reduce((sum, num) => sum + num, 0);
   }
 }
-
-// console.log(add(""));
-// console.log(add("1"));
-// console.log(add("1,5"));
